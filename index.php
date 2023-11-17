2ac235f7d97d7863862bd58e145c5e1a9ee5f0c7
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-11-18 01:57:37 +0700

    rename dan update file halaman beranda

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lindungi Bumi</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="assets/style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400&display=swap"
+        rel="stylesheet">
+</head>
+
+<body class="bg-[#217e6b]">
+    <nav class="flex justify-between bg-[#002633] w-full sticky top-0">
+        <!-- logo -->
+        <section class="w-max box-border">
+            <a href="index.php">
+                <img src="assets/image/logo.png" alt="Logo" class="w-[100px]">
+            </a>
+        </section>
+
+        <!-- navbar list -->
+        <section class=" w-3/6 box-border">
+            <ul class="flex justify-around items-center text-[#3dc6aa] text-lg h-full font-[Poppins]">
+                <li class="px-2 hover:bg-slate-600 hover:delay-75 hover:border border-gray-400 rounded-lg">
+                    <a href="index.php">Beranda</a>
+                </li>
+                <li class="px-2 hover:bg-slate-600 hover:delay-75 hover:border border-b-gray-400 rounded-lg">
+                    <a href="index.php">Ajukan
+                        Pengaduan</a>
+                </li>
+                <li class="px-2 hover:bg-slate-600 hover:delay-75 hover:border border-b-gray-400 rounded-lg">
+                    <a href="index.php">Daftar
+                        Pengaduan</a>
+                </li>
+                <li class="px-2 hover:bg-slate-600 hover:delay-75 hover:border border-b-gray-400 rounded-lg">
+                    <a href="index.php">Masuk / Daftar</a>
+                </li>
+            </ul>
+        </section>
+    </nav>
+
+    <article class="w-[100%] m-auto h-full">
+        <!-- gambar sampah -->
+        <section class="flex w-full h-full border shadow-picture">
+            <img src="assets/image/sampah1.png" alt="Sampah di pantai" class="w-[40%] h-[300px]">
+            <img src="assets/image/sampah2.png" alt="Sampah di jalan" class="w-[40%] h-[300px]">
+            <img src="assets/image/sampah3.png" alt="Sampah di jalan" class="w-[40%] h-[300px]">
+        </section>
+
+        <h1 class="text-5xl font-bold text-white text-center mt-10">Bumi sekarang semakin tercemar</h1>
+        <!-- Tulisan tulisan -->
+        <section>
+
+            <h2>Mari bersama-sama selamatkan bumi</h2>
+            <h2>Sampah di sekitar anda yang sudah menumpuk</h2>
+            <p>"Ajukan pengaduan sekarang ke pihak berwenang agar di ekskusi secepatnya"</p>
+        </section>
+        <button>Ajukan Pengaduan</button>
+    </article>
+
+    <footer></footer>
+</body>
+
+</html>
