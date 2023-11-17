185443518b253d0d26f1409b4639c2479569cfa2
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-11-18 01:57:12 +0700

    edit config tailwind

---
+++ assets/script/tailwind.config.js
@@ -0,0 +1,16 @@
+
+// module.exports = {
+//     content: ['../../*.{php}'],
+//     theme: {
+//         colors: {
+//             'greenBackground': '#3dc6aa'
+//         },
+//         fontFamily: {
+//             'sans': ['Helvetica', 'Arial', 'sans-serif'],
+//             'serif': ['Georgia', 'serif'],
+//             'mono': ['Courier', 'monospace'],
+//             'custom': ['Your Custom Font', 'sans-serif'], // Ganti 'Your Custom Font' dengan nama font yang ingin kamu tambahkan
+//         },
+//     },
+//     extend: {},
+// };
