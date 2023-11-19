be9f7fea8b10f00b640650a0f51186f86c8c5f20
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-11-19 10:06:27 +0700

    membuat file untuk connect ke DB

---
+++ BE_database.php
@@ -0,0 +1,12 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "lindungi_bumi";
+
+$konek = new mysqli($hostname, $username, $password, $database);
+
+if ($konek->connect_error) {
+    die('Maaf koneksi gagal: ' . $konek->connect_error);
+}
