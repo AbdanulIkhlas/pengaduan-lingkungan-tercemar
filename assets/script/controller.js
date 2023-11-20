605ed4cab598c913586f52312bc128ef7daf4632
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-11-20 22:39:23 +0700

    membuat script untuk modal

---
+++ assets/script/controller.js
@@ -0,0 +1,19 @@
+function openDeleteModal() {
+    // Set parameter (e.g., ID Pengaduan) pada modal
+    // Tambahkan logika lainnya sesuai kebutuhan
+    document.getElementById('modalDelete').style.display = 'block';
+}
+
+function closeDeleteModal() {
+    document.getElementById('modalDelete').style.display = 'none';
+}
+
+function openEksekusiModal() {
+    // Set parameter (e.g., ID Pengaduan) pada modal
+    // Tambahkan logika lainnya sesuai kebutuhan
+    document.getElementById('modalTindakLanjut').style.display = 'block';
+}
+
+function closeEksekusiModal() {
+    document.getElementById('modalTindakLanjut').style.display = 'none';
+}
